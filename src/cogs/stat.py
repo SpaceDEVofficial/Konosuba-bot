892a8a65b6e8de742c24553096e1c5a1377d9618
popop098 <popop098@naver.com>
2021-09-17 17:31:12 +0900

    :fire: something add

--- src/cogs/stat.py
+++ src/cogs/stat.py
@@ -11,8 +11,8 @@ class MyStatcordCog(commands.Cog):
     """
     def __init__(self, bot):
         self.bot = bot
-        self.kb = koreanbots.Koreanbots(self.bot, 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg4NTcxMjY4MTQ5ODIxNDQ1MCIsImlhdCI6MTYzMTcxNzIxN30.dhlnG2Pg5CpPltiScaFqBLApqFT5yb3ZDEqCSXrxysmclsXevi3Tc07_3clw3cAgRCFbbzKmqdphrk7dJid6TnW4SFEISb-_CilRn4OmbC-BP_ckeNuoVnw7VjJXuOm0rhTdmM7HQOVpKpQZ8RCR2cv1dpn7_riV04Q4gCSGCvM', run_task=True)
-        self.ub = UniqueBotsKR.client(self.bot, token='OHVXeHM1ZHR6Qm1RMERnRWJndGd1by9neklvcENiaEhweDU1NDRwczJ6NzhIQVlwL0FEWXZCQzM3NXNUM1dlN1d4V1ozWExFMUhlTXE4N3VaSFVnWlE9PQ==',autopost=True)
+        self.kb = koreanbots.Koreanbots(self.bot, os.getenv("KBTOKEN"), run_task=True)
+        self.ub = UniqueBotsKR.client(self.bot, token=os.getenv("UBTOKEN"),autopost=True)
 
 def setup(bot):
     bot.add_cog(MyStatcordCog(bot))
