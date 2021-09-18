9c62ed9d77d22086419df37208be41f319d15c17
popop098 <popop098@naver.com>
2021-09-18 11:10:50 +0900

    :sparkles: fix server count chart!

--- src/utils/guild_count_loop.py
+++ src/utils/guild_count_loop.py
@@ -1,27 +1,32 @@
 import datetime
 import time
-
-from async_task_helpers import loop
+from discord.ext import tasks
 import aiohttp
 import asyncio
 import aiosqlite
 
+sql_create_initial_thunder_table = """ CREATE TABLE IF NOT EXISTS guild_count (
+
+                                    counts integer PRIMARY KEY AUTOINCREMENT,
+
+                                    dates text DEFAULT (datetime('now','localtime'))
+
+                                ); """
+
+@tasks.loop(minutes=10)
 async def loops():
     async with aiohttp.ClientSession() as cs:
         async with cs.get("https://koreanbots.dev/api/v2/bots/885712681498214450") as res:
             re = await res.json()
             servs = re["data"]["servers"]
-            date = datetime.datetime.now()
-            date_text = f"{date.year}/{date.month}/{date.day} {date.hour}:{date.minute}"
+            #date = datetime.datetime.now()
+            #date_text = f"{date.year}-{date.month}-{date.day} {date.hour}:{date.minute}"
             async with aiosqlite.connect("db/db.db") as con:
-                await con.execute("INSERT INTO guild_count VALUES (?,?)",(date_text,servs))
+                await con.execute(sql_create_initial_thunder_table)
+                await con.execute(f"INSERT INTO guild_count(counts) VALUES (?)",(servs,))
                 await con.commit()
             print(f"Updated. Servers: {servs}")
-    await asyncio.sleep(60*10)
 
-async def main():
-    task = await asyncio.create_task(loops())
+loops.start()
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+asyncio.get_event_loop().run_forever()
--- src/utils/guild_count_server.py
+++ src/utils/guild_count_server.py
@@ -5,21 +5,24 @@ import quart
 from quart import request
 from io import BytesIO
 import aiosqlite
-
+import math
 async def out():
     async with aiosqlite.connect("db/db.db") as con:
         data = await (await con.execute("SELECT * FROM guild_count")).fetchall()
         x = []
         y = []
         for i in data:
-            x.append(i[0])
-            y.append(i[1])
+            x.append(i[1][:-3])
+            y.append(i[0])
         #x = [datetime.strptime(date, "%Y/%m/%d %H:%M").date() for date in datas]
         print(y,x)
         fig, ax = plt.subplots()
         ax.set(title='Konosuba bot Server chart!')
         plt.plot_date(x, y, linestyle='solid')
-        plt.gcf().set_size_inches(8, 6)
+        plt.gcf().set_size_inches(20, 10)
+        new_list = range(math.floor(min(y)), math.ceil(max(y)) + 1)
+        plt.yticks(new_list)
+        ax.xaxis.set_tick_params(rotation=35, labelsize=10)
         bytesio = BytesIO()
         plt.savefig(bytesio, dpi=300, format='png')
         bytesio.seek(0)
